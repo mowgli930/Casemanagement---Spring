78e2add521776bd6d2feb53a74ca0ce92bf1fe8f
Joakim Landström <joakim.landstrom@outlook.com>
2016-11-03 15:10:51 +0100

    formatting

--- casemanagement/src/main/java/se/plushogskolan/casemanagement/model/User.java
+++ casemanagement/src/main/java/se/plushogskolan/casemanagement/model/User.java
@@ -20,187 +20,186 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class User extends AbstractEntity {
 
-    @Column(unique = true)
-    private String username;
+	@Column(unique = true)
+	private String username;
 
-    @ManyToOne
-    private Team team;
+	@ManyToOne
+	private Team team;
 
-    @OneToMany
-    private WorkItem workItem;
+	@OneToMany
+	private WorkItem workItem;
 
-    private boolean isActive;
+	private boolean isActive;
 
-    private String firstName;
+	private String firstName;
 
-    private String lastName;
+	private String lastName;
 
-    @CreatedBy
-    private String createdBy;
+	@CreatedBy
+	private String createdBy;
 
-    @LastModifiedBy
-    private String lastModifiedBy;
+	@LastModifiedBy
+	private String lastModifiedBy;
 
-    @CreatedDate
-    private Date createdDate;
+	@CreatedDate
+	private Date createdDate;
 
-    @LastModifiedDate
-    private Date lastModifiedDate;
+	@LastModifiedDate
+	private Date lastModifiedDate;
 
-    private User(boolean isActive, String username, String firstName, String lastName, Team team, WorkItem workItem) {
+	private User(boolean isActive, String username, String firstName, String lastName, Team team, WorkItem workItem) {
 
-	this.isActive = isActive;
-	this.username = username;
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.team = team;
-	this.workItem = workItem;
-
-    }
-
-    protected User() {
-    }
-
-    public static UserBuilder builder() {
-	return new UserBuilder();
-    }
-
-    public boolean isIdentical(Object other) {
-	if (!equals(other)) {
-	    return false;
-	}
-	User otherUser = (User) other;
-	return firstName.equals(otherUser.firstName) && lastName.equals(otherUser.lastName) && team == otherUser.team
-		&& isActive == otherUser.isActive;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-
-	if (this == other) {
-	    return true;
+		this.isActive = isActive;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.team = team;
+		this.workItem = workItem;
 	}
 
-	if (null == other) {
-	    return false;
+	protected User() {
 	}
 
-	if (other instanceof User) {
-	    User otherUser = (User) other;
-	    return id == otherUser.getId() && username.equals(otherUser.getUsername());
-	}
-	return false;
-    }
-
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((username == null) ? 0 : username.hashCode());
-	return result;
-    }
-
-    @Override
-    public String toString() {
-	return ToStringBuilder.reflectionToString(ToStringStyle.JSON_STYLE);
-    }
-
-    public boolean isActive() {
-	return isActive;
-    }
-
-    public Team getTeam() {
-	return team;
-    }
-
-    public WorkItem getWorkItem() {
-	return workItem;
-    }
-
-    public String getUsername() {
-	return username;
-    }
-
-    public String getFirstName() {
-	return firstName;
-    }
-
-    public String getLastName() {
-	return lastName;
-    }
-
-    public String getCreatedBy() {
-	return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-	this.createdBy = createdBy;
-    }
-
-    public String getLastModifiedBy() {
-	return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-	this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public Date getCreatedDate() {
-	return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-	this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-	return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-	this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public static final class UserBuilder {
-
-	private Team team = null;
-	private WorkItem workItem = null;
-	private boolean isActive = true;
-	private String firstName = "";
-	private String lastName = "";
-
-	private UserBuilder() {
-	    super();
+	public static UserBuilder builder() {
+		return new UserBuilder();
 	}
 
-	public User build(String username) {
-
-	    return new User(isActive, username, firstName, lastName, team, workItem);
+	public boolean isIdentical(Object other) {
+		if (!equals(other)) {
+			return false;
+		}
+		User otherUser = (User) other;
+		return firstName.equals(otherUser.firstName) && lastName.equals(otherUser.lastName) && team == otherUser.team
+				&& isActive == otherUser.isActive;
 	}
 
-	public UserBuilder setActive(boolean isActive) {
-	    this.isActive = isActive;
-	    return this;
+	@Override
+	public boolean equals(Object other) {
+
+		if (this == other) {
+			return true;
+		}
+
+		if (null == other) {
+			return false;
+		}
+
+		if (other instanceof User) {
+			User otherUser = (User) other;
+			return id == otherUser.getId() && username.equals(otherUser.getUsername());
+		}
+		return false;
 	}
 
-	public UserBuilder setFirstName(String firstName) {
-	    this.firstName = firstName;
-	    return this;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
 	}
 
-	public UserBuilder setLastName(String lastName) {
-	    this.lastName = lastName;
-	    return this;
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(ToStringStyle.JSON_STYLE);
 	}
 
-	public UserBuilder setTeam(Team team) {
-	    this.team = team;
-	    return this;
+	public boolean isActive() {
+		return isActive;
 	}
 
-	public UserBuilder setWorkItem(WorkItem workItem) {
-	    this.workItem = workItem;
-	    return this;
+	public Team getTeam() {
+		return team;
 	}
-    }
+
+	public WorkItem getWorkItem() {
+		return workItem;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public static final class UserBuilder {
+
+		private Team team = null;
+		private WorkItem workItem = null;
+		private boolean isActive = true;
+		private String firstName = "";
+		private String lastName = "";
+
+		private UserBuilder() {
+			super();
+		}
+
+		public User build(String username) {
+
+			return new User(isActive, username, firstName, lastName, team, workItem);
+		}
+
+		public UserBuilder setActive(boolean isActive) {
+			this.isActive = isActive;
+			return this;
+		}
+
+		public UserBuilder setFirstName(String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
+
+		public UserBuilder setLastName(String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+
+		public UserBuilder setTeam(Team team) {
+			this.team = team;
+			return this;
+		}
+
+		public UserBuilder setWorkItem(WorkItem workItem) {
+			this.workItem = workItem;
+			return this;
+		}
+	}
 }
