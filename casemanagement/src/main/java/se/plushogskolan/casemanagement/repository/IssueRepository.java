bd8607ad8c91454cbd40777b964a3acbf6459c68
leo <leo.moralesvega@gmail.com>
2016-11-14 12:59:34 +0100

    getIssuesByWorkItemId in IssueRepository now returns Slice<Issue> instead of List<Issue>

--- casemanagement/src/main/java/se/plushogskolan/casemanagement/repository/IssueRepository.java
+++ casemanagement/src/main/java/se/plushogskolan/casemanagement/repository/IssueRepository.java
@@ -1,9 +1,7 @@
 package se.plushogskolan.casemanagement.repository;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +11,5 @@ import se.plushogskolan.casemanagement.model.Issue;
 public interface IssueRepository extends PagingAndSortingRepository<Issue, Long>{
 
 	@Query("SELECT i FROM #{#entityName} i WHERE i.workItem.id = :workItemId")
-    List<Issue> getIssuesByWorkItemId(@Param("workItemId")Long workItemId);    
+    Slice<Issue> getIssuesByWorkItemId(@Param("workItemId")Long workItemId, Pageable pageable);    
 }
